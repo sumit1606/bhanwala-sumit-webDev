feb39a46bcbd03b09a0158bcc10df8113e479ddb
sumitb1606 <sumitbhanwala@Sumits-MacBook-Pro.local>
2017-03-21 20:38:35 -0400

    Adding user model

--- assignment/model/user/user.model.server.js
+++ assignment/model/user/user.model.server.js
@@ -5,40 +5,30 @@ module.exports = function () {
 
      var api = {
          createUser: createUser ,
-        // findUserById : findUserById,
-        // findUserByUsername : findUserByUsername,
-        // findUserByCredentials : findUserByCredentials,
+         findUserById : findUserById,
+         findUserByUsername : findUserByUsername,
+         findUserByCredentials : findUserByCredentials,
+         updateUser : updateUser,
+         deleteUser : deleteUser
      };
 
     var mongoose = require('mongoose');
-
     var UserSchema = require('./user.schema.server.js')();
     var UserModel = mongoose.model('UserModel', UserSchema);
 
     return api;
-    //
-    // var mongoose = require('mongoose');
-    // var userSchema = mongoose.Schema({
-    //     userName : {type :String , required: true},
-    //     password : {type :String, required : true},
-    //     firstName : String,
-    //     lastName :  String,
-    //     email : String,
-    //     phone : String,
-    //     dateCreated : {type : Date , default :Date.now()}
-    // },{collection: 'users'});
 
      function createUser(user) {
          return UserModel.create(user);
      }
-     // function findUserById(user) {
-     //     return UserModel.create(user);
-     // }
-     // function findUserByUsername(user) {
-     //     return UserModel.create(user);
-     // }
-     //
-     // function findUserByCredentials(user) {
-     //     return UserModel.create(user);
-     // }
+      function findUserById(user) {
+         return UserModel.create(user);
+      }
+      function findUserByUsername(user) {
+          return UserModel.create(user);
+      }
+
+      function findUserByCredentials(user) {
+          return UserModel.create(user);
+      }
 };
--- assignment/model/user/user.schema.server.js
+++ assignment/model/user/user.schema.server.js
@@ -5,13 +5,15 @@ module.exports = function () {
     var mongoose = require('mongoose');
     var userSchema = mongoose.Schema({
         username : {type :String , required: true},
-        password : {type :String, required : true},
+        password : String,
         firstName : String,
         lastName :  String,
         email : String,
         phone : String,
-        dateCreated : {type : Date , default :Date.now()}
-    },{collection: 'users'});
-
+        dateCreated : {type : Date , default :Date.now()},
+        websites :[{type :mongoose.Schema.Types.ObjectId , ref:'websiteModel'}]
+    },{collection: 'user'});
+// website modal is the schema that is implemented to give the
+// websites that are related to this particular user
     return userSchema;
 };
